36eb45824d51c370c59d578a3efc24e5bb84222f
shakedm100 <shaked1mi@gmail.com>
2025-05-19 14:19:26 +0300

    Added many categories to Category. Maybe will be removed and remain DB only

--- WorkOut/app/src/main/java/Model/Category.java
+++ WorkOut/app/src/main/java/Model/Category.java
@@ -2,5 +2,29 @@ package Model;
 
 public enum Category
 {
-
+    Bicycle,
+    Boxing,
+    MartialArts,
+    Badminton,
+    Baseball,
+    Bridge,
+    Judo,
+    Golf,
+    IceHockey,
+    Wrestling,
+    Taekwondo,
+    Climbing,
+    Tennis,
+    Soccer,
+    Handball,
+    Basketball,
+    Volleyball,
+    BeachVolleyball,
+    Fencing,
+    Squash,
+    Football,
+    Karate,
+    Archery,
+    Rugby,
+    Swimming
 }
