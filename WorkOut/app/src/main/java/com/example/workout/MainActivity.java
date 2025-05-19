2c1eff1c0aa83058c45d016261db0e824f8c9a79
shakedm100 <shaked1mi@gmail.com>
2025-05-19 14:56:59 +0300

    Fixed moving between actives from home screen of a client

--- WorkOut/app/src/main/java/com/example/workout/MainActivity.java
+++ WorkOut/app/src/main/java/com/example/workout/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.workout;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+import android.view.MenuItem;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,39 +14,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page); // בחר את ה־XML המתאים
+        setContentView(R.layout.home_page); // Choose the correct XML
 
-        // מציאת ה־BottomNavigationView
+        // Find the BottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        // הגדרת מאזין ללחיצות על הכפתורים
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_home:
-                        // טיפול בלחיצה על בית
-                        return true;
-                    case R.id.nav_profile:
-                        // מעבר לפרופיל
-                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-                        return true;
-                    case R.id.nav_search:
-                        // חיפוש
-                        startActivity(new Intent(MainActivity.this, SearchActivity.class));
-                        return true;
-                    case R.id.nav_ratings:
-                        // דירוגים
-                        startActivity(new Intent(MainActivity.this, RatingsActivity.class));
-                        return true;
-                    case R.id.nav_messages:
-                        // הודעות
-                        startActivity(new Intent(MainActivity.this, MessagesActivity.class));
-                        return true;
-                    default:
-                        return false;
-                }
+        // Setup event listener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                // you’re already here
+                return true;
             }
+            else if (id == R.id.nav_profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            }
+            else if (id == R.id.nav_search) {
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;
+            }
+            else if (id == R.id.nav_ratings) {
+                startActivity(new Intent(this, RatingsActivity.class));
+                return true;
+            }
+            else if (id == R.id.nav_messages) {
+                startActivity(new Intent(this, MessagesActivity.class));
+                return true;
+            }
+            return false;
         });
+
+
     }
 }
