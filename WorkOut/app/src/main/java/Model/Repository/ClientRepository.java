d8e82421640e5995ad6592e9d916965feea77b47
shakedm100 <shaked1mi@gmail.com>
2025-05-23 17:50:34 +0300

    Implemented get, update and delete for ClientRepository

--- WorkOut/app/src/main/java/Model/Repository/ClientRepository.java
+++ WorkOut/app/src/main/java/Model/Repository/ClientRepository.java
@@ -1,12 +1,9 @@
 package Model.Repository;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.Filter;
@@ -24,13 +21,28 @@ import Model.Address;
 
 public class ClientRepository
 {
-    private FirebaseFirestore db;
+    private final FirebaseFirestore db;
 
     public ClientRepository()
     {
         db = FirebaseFirestore.getInstance();
     }
 
+    /**
+     * This function is responsible for the logic of client insertion to the database.
+     * It returns an instance of the new client if it succeeded with the new client id.
+     * If communication with the DB fails it throws an exception.
+     * If the username or email already exist it also throws an exception.
+     * @param username user's username
+     * @param password user's password
+     * @param phone user's phone
+     * @param email user's email
+     * @param firstName user's firstname
+     * @param lastName user's password
+     * @param address user's address
+     * @param gender user's gender
+     * @return a new client instance if succeeded, exception otherwise
+     */
     public Task<Client> insertClient(String username, String password, Phone phone, String email, String firstName,
                                      String lastName, Address address, Gender gender)
     {
@@ -74,7 +86,16 @@ public class ClientRepository
     }
 
 
-    public Task<Boolean> canRegisterUser(String username, String email)
+    /**
+     * This method receives username and email and check if one of them already exists
+     * in the database, if one of them exist it returns true async
+     * and if not it returns false async
+     * throws an error if communication failed
+     * @param username the user's username
+     * @param email the user's email
+     * @return Task<false> if the user's username & email don't exit. true otherwise
+     */
+    private Task<Boolean> canRegisterUser(String username, String email)
     {
         return db.collection("clients")
                 .where(
@@ -94,4 +115,67 @@ public class ClientRepository
                     return snap != null && !snap.isEmpty();
                 });
     }
+
+    public Task<Boolean> updateClientByID(Client client)
+    {
+        // Get the client's DocumentReference
+        DocumentReference currentClient = db.collection("clients").document(client.getId());
+
+        return currentClient.set(client).continueWith(task -> task.isSuccessful());
+    }
+
+    public Task<Boolean> deleteClientByID(Client client)
+    {
+        DocumentReference currentClient = db.collection("clients").document(client.getId());
+
+        return currentClient.delete().continueWith(task -> task.isSuccessful());
+    }
+
+    public Task<Client> getClientByUsername(String username)
+    {
+        return db.collection("clients")
+                .whereEqualTo("username", username)
+                .limit(1)
+                .get()
+                .continueWith(task -> {
+                    if (!task.isSuccessful()) {
+                        // bubble up Firestore errors
+                        throw Objects.requireNonNull(task.getException());
+                    }
+                    QuerySnapshot snap = task.getResult();
+                    if (snap == null || snap.isEmpty()) {
+                        // no user found
+                        throw new IllegalArgumentException(
+                                "No client with username: " + username);
+                    }
+
+                    // grab the first (and only) document
+                    DocumentSnapshot doc = snap.getDocuments().get(0);
+
+                    // If this doesn't work we can try the other method in the comments
+                    Client client = doc.toObject(Client.class);
+                    if (client == null) {
+                        throw new IllegalStateException(
+                                "Failed to map document to Client");
+                    }
+                    client.setId(doc.getId());
+                    return client;
+                /*
+                Another option if the first one doesn't work:
+                 String id = doc.getId();
+                 String user = doc.getString("username");
+                 String password = doc.getString("password");
+                 Phone phone = doc.toObject(Phone.class);     // or doc.get("phone", Phone.class);
+                 String email = doc.getString("email");
+                 String firstName = doc.getString("firstName");
+                 String lastName = doc.getString("lastName");
+                 Address address = doc.toObject(Address.class);
+                 Gender gender = doc.get("gender", Gender.class);
+
+                 return new Client(id, usr, pwd, phone, email, firstName, lastName, address, gender);
+                 */
+        });
+    }
+
+
 }
