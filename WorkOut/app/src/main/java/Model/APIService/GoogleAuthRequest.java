0436f648d07069c700ac31784dcebada154747bf
shakedm100 <shaked1mi@gmail.com>
2025-05-23 19:29:18 +0300

    Deleted unused Classes

--- WorkOut/app/src/main/java/Model/APIService/GoogleAuthRequest.java
+++
@@ -1,8 +0,0 @@
-package Model.APIService;
-
-public class GoogleAuthRequest
-{
-    private String idToken;
-    public GoogleAuthRequest(String idToken) { this.idToken = idToken; }
-    public String getIdToken() { return idToken; }
-}
