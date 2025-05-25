4ccd7654ff642a0c0516f5e7978903df7ab5dd86
shakedm100 <shaked1mi@gmail.com>
2025-05-25 17:15:20 +0300

    Increased SDK to 26 to enable Parse

--- WorkOut/app/build.gradle.kts
+++ WorkOut/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.workout"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
