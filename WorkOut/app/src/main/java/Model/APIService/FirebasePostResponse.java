d553822697b3a8fed281dd54052a591c2fc65b7a
shakedm100 <shaked1mi@gmail.com>
2025-05-27 19:08:00 +0300

    Old unused classes

--- WorkOut/app/src/main/java/Model/APIService/FirebasePostResponse.java
+++
@@ -1,9 +0,0 @@
-package Model.APIService;
-
-import com.google.gson.annotations.SerializedName;
-
-public class FirebasePostResponse {
-    // the automatically generated key for your new node
-    @SerializedName("name")
-    public String key;
-}
