7606adfb563c745dba50972353634c6e29c469bf
DvirBt <dvirbto@gmail.com>
2025-08-23 22:04:07 +0300

    RatingViewModel update

--- WorkOut/app/src/main/java/ViewModel/ClientRatingViewModel.java
+++ WorkOut/app/src/main/java/ViewModel/ClientRatingViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
 
 import Model.Business;
 import Model.Client;
@@ -17,6 +18,7 @@ public class ClientRatingViewModel extends ViewModel
     private final ClientRepository clientRepository;
     private final BusinessRepository businessRepository;
     private final MutableLiveData<GenericUiState<String>> ratingsUiState = new MutableLiveData<>(GenericUiState.idle());
+    private TaskCompletionSource<Boolean> taskCompletionSource;
 
     /**
      * Default constructor for the ClientRatingViewModel.
@@ -35,49 +37,88 @@ public class ClientRatingViewModel extends ViewModel
         return ratingsUiState;
     }
 
-    public Task<Business> addRating(float stars, String comment, Client client, Business business)
+    public boolean basicCheck(float stars, String comment, Client client, Business business)
     {
         // check validation
         if (stars < 0.5)
         {
             ratingsUiState.postValue(GenericUiState.error("Rating must be at least 0.5 stars."));
-            return null;
+            return false;
         }
 
         if (comment.trim().isEmpty())
         {
             ratingsUiState.postValue(GenericUiState.error("Please enter a comment."));
-            return null;
+            return false;
         }
 
-        Rating rating = new Rating(stars, comment, client);
+        // TODO: check if the client was registered to this business course?
+        if (client == null)
+        {
+            ratingsUiState.postValue(GenericUiState.error("Please enter a comment."));
+            return false;
+        }
 
-        return businessRepository.addRatingToBusiness(business, rating)
-                .addOnSuccessListener(task ->
-                {
-                    ratingsUiState.postValue(GenericUiState.success("Rating success!"));
-                })
-                .addOnFailureListener(insertException ->
-                {
-                    ratingsUiState.postValue(GenericUiState.error("Rating failed: " + insertException.getMessage()));
-                });
+        if (business == null)
+        {
+            ratingsUiState.postValue(GenericUiState.error("Please enter a comment."));
+            return false;
+        }
+
+        return true;
+    }
+    public Task<Boolean> addRating(float stars, String comment, Client client, Business business)
+    {
+        taskCompletionSource = new TaskCompletionSource<>();
+
+        if (!basicCheck(stars, comment, client, business))
+            taskCompletionSource.setResult(false);
+
+        else
+        {
+            Rating rating = new Rating(stars, comment, client);
+            businessRepository.addRatingToBusiness(business, rating)
+                    .addOnSuccessListener(task ->
+                    {
+                        ratingsUiState.postValue(GenericUiState.success("Rating success!"));
+                        taskCompletionSource.setResult(true);
+                    })
+                    .addOnFailureListener(insertException ->
+                    {
+                        ratingsUiState.postValue(GenericUiState.error("Rating failed: " + insertException.getMessage()));
+                        taskCompletionSource.setResult(false);
+                    });
+        }
+
+        return taskCompletionSource.getTask();
     }
 
-    public Task<Business> updateRating(Business business, Rating rating)
+    public Task<Boolean> updateRating(Business business, Rating rating)
     {
+        taskCompletionSource = new TaskCompletionSource<>();
+
         if(business == null || rating == null)
         {
             ratingsUiState.postValue(GenericUiState.error("Error receiving business or rating"));
-            return null;
+            taskCompletionSource.setResult(false);
         }
 
-        return businessRepository.updateRatingFromBusiness(business, rating).addOnSuccessListener(task ->
+        else
         {
-            ratingsUiState.postValue(GenericUiState.success("Rating update success!"));
-        }).addOnFailureListener(insertException ->
-        {
-            ratingsUiState.postValue(GenericUiState.error("Rating update failed: " + insertException.getMessage()));
-        });
+            businessRepository.updateRatingFromBusiness(business, rating)
+                    .addOnSuccessListener(task ->
+                    {
+                        ratingsUiState.postValue(GenericUiState.success("Rating update success!"));
+                        taskCompletionSource.setResult(true);
+                    })
+                    .addOnFailureListener(insertException ->
+                    {
+                        ratingsUiState.postValue(GenericUiState.error("Rating update failed: " + insertException.getMessage()));
+                        taskCompletionSource.setResult(false);
+                    });
+        }
+
+        return taskCompletionSource.getTask();
     }
 
     public Task<Rating> checkIfRatingExists(Business business, Client client)
@@ -90,10 +131,7 @@ public class ClientRatingViewModel extends ViewModel
 
         return businessRepository.checkIfRatingExists(business, client).addOnSuccessListener(task ->
         {
-            if(task != null)
-                ratingsUiState.postValue(GenericUiState.idle());
-            else
-                ratingsUiState.postValue(GenericUiState.idle());
+            ratingsUiState.postValue(GenericUiState.idle());
         }).addOnFailureListener(insertException ->
         {
             ratingsUiState.postValue(GenericUiState.error("Checking for existing rating failed: " + insertException.getMessage()));
