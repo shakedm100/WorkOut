f13e71f1e1b9b3a83edf347046de62884838b6cb
shakedm100 <shaked1mi@gmail.com>
2025-08-31 16:33:18 +0300

    Fixed a bug that the adapter displayed the wrong time

--- WorkOut/app/src/main/java/Adapters/CourseAdapter.java
+++ WorkOut/app/src/main/java/Adapters/CourseAdapter.java
@@ -49,7 +49,7 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
         int duration = course.getDuration();
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(occurrence);
-        calendar.add(Calendar.HOUR, 2);
+        calendar.add(Calendar.HOUR, -2);
         Date startTime = calendar.getTime();
         calendar.add(Calendar.MINUTE, duration);
         Date endTime = calendar.getTime();
